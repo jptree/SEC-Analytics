b892cea5f95799da03dcb72e148b9519221b07b0
Jason Petri <jasonfpetri@outlook.com>
2021-05-18 14:48:20 -0400

    s

--- Analysis/featureEngineering.py
+++ Analysis/featureEngineering.py
@@ -10,12 +10,21 @@ import matplotlib.pyplot as plt
 
 def get_raw_mda():
     bankruptcy = pd.read_csv('bankruptcy_cik.csv', index_col=0)
+    # Get annual files
     _, _, files = next(os.walk(f'Extracted'))
     mda = pd.DataFrame(columns=['date', 'cik', 'mda'])
     for file in files:
         extracted = pd.read_csv(f'Extracted\\{file}', header=None,
                                 names=['path', 'date', 'cik', 'mda'])
         mda = mda.append(extracted[['date', 'cik', 'mda']], ignore_index=True)
+
+    # Get quarterly files
+    _, _, files = next(os.walk(f'Extracted\\Quarterly'))
+    for file in files:
+        extracted = pd.read_csv(f'Extracted\\Quarterly\\{file}', header=None,
+                                names=['path', 'date', 'cik', 'mda'])
+        mda = mda.append(extracted[['date', 'cik', 'mda']], ignore_index=True)
+
     mda['merge_date'] = mda['date'].apply(lambda x: f'{str(x)[:4]}-{str(x)[4:6]}')
     all = pd.merge(mda, bankruptcy, left_on=['merge_date', 'cik'], right_on=['date_month', 'cik'])
     return all
@@ -89,27 +98,37 @@ def lda_stuff(corpus):
                               columns=['topic', 'id', 'word', 'weight'])
     dtf_topics.to_csv('topics.csv')
     ## plot
-    fig, ax = plt.subplots()
-    sns.barplot(y="word", x="weight", hue="topic", data=dtf_topics, dodge=False, ax=ax).set_title('Main Topics')
-    ax.set(ylabel="", xlabel="Word Importance")
-    plt.show()
+    # fig, ax = plt.subplots()
+    # sns.barplot(y="word", x="weight", hue="topic", data=dtf_topics, dodge=False, ax=ax).set_title('Main Topics')
+    # ax.set(ylabel="", xlabel="Word Importance")
+    # plt.show()
+
+
+def surrounding_word_frequencies(word, corpus):
+
+    for document in corpus:
+        docs = document.split(' ')
+
+
 
 if __name__ == "__main__":
     # Obtain the extracted MD&A sections in dataframe format
-    # data = get_raw_mda()
-    _, _, file_path = next(os.walk('../Data/10-K Sample'))
-    data = [open(f'../Data/10-K Sample/{file}').read() for file in file_path]
+    data = get_raw_mda()
+    # _, _, file_path = next(os.walk('../Data/10-K Sample'))
+    # data = [open(f'../Data/10-K Sample/{file}').read() for file in file_path]
 
     # Clean the raw text
     nltk_stopwords = nltk.corpus.stopwords.words("english")
     nltk_lemmatizer = nltk.stem.wordnet.WordNetLemmatizer()
     # corpus = mp_handler(data, nltk_lemmatizer, nltk_stopwords, 2)
-    # corpus = mp_handler(list(data['mda']), nltk_lemmatizer, nltk_stopwords, 1)
+    corpus = mp_handler(list(data['mda']), nltk_lemmatizer, nltk_stopwords, 4)
 
-    financial_stopwords = ['january', 'februrary', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
-                           'november', 'december', 'year', 'fiscal']
+    # financial_stopwords = ['january', 'februrary', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
+    #                        'november', 'december', 'year', 'fiscal']
 
-    print(nltk_stopwords)
+    lda_stuff(corpus)
+
+    # print(nltk_stopwords)
 
 
 
